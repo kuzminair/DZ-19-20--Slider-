289369f0beec3485c7edb6b87d1bea77c709b68b
Irina Kuzmina <i.kuzmina@list.ru>
2023-08-01 23:15:45 +0500

    изменения 1 авг

--- scripts/index.js
+++ scripts/index.js
@@ -4,28 +4,28 @@ var red = document.getElementById('red');
 var blue = document.getElementById('blue');
 
 function chahgeSlider() {
-    if(parseInt(slider.value)>50){
-        container.style.backgroundColor='white';
-        // container.style.alignItems = 'normal';
+    if(parseInt(slider.value)>128){
+        container.style.backgroundColor=`rgb(${slider.value}, ${slider.value}, ${slider.value})`;
         red.style.position = 'absolute';
         blue.style.position = 'absolute';
-        red.style.left = '0px';
-        blue.style.right = '0px';
-        blue.style.bottom = '0px';
+        let redleft = (container.clientWidth*slider.value)/255-300;
+        red.style.left = `${redleft}px`;
+        let blueleft = (container.clientWidth*(255-slider.value))/255-55;
+        blue.style.left = `${blueleft}px`;
     }
-    if(parseInt(slider.value)==50){
-        container.style.backgroundColor='darkgrey';
+    if(parseInt(slider.value)==128){
+        container.style.backgroundColor=`rgb(${slider.value}, ${slider.value}, ${slider.value})`;
         container.style.alignItems = 'center';
         red.style.position = 'static';
         blue.style.position = 'static';
     }
-    if(parseInt(slider.value)<50){
-        container.style.backgroundColor='black';
-        // container.style.alignItems = 'center';
+    if(parseInt(slider.value)<128){
+        container.style.backgroundColor=`rgb(${slider.value}, ${slider.value}, ${slider.value})`;
         red.style.position = 'absolute';
         blue.style.position = 'absolute';
-        red.style.right = '0px';
-        blue.style.left = '0px';
-        blue.style.bottom = '0px';
+        let blueleft = (container.clientWidth*slider.value)/255-300;
+        blue.style.left = `${blueleft}px`;        
+        let redleft = (container.clientWidth*(255-slider.value))/255-55;
+        red.style.left = `${redleft}px`;
     }
 }
